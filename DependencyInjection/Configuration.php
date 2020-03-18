d9f9bce90bde24d74601c510ac6f23c851548f35
Sébastien HEITZMANN <2le@2le.net>
2020-03-18 14:05:42 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('tkuska_dashboard');
+        $treeBuilder = new TreeBuilder('tkuska_dashboard');
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
